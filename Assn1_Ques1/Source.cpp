64f580013097e5fac3c02650c35b8f64f81888cd
Phil Fernandez <norcal618@live.com>
2016-08-28 14:09:42 -0700

    ok

--- Assn1_Ques1/Source.cpp
+++ Assn1_Ques1/Source.cpp
@@ -5,8 +5,7 @@ using namespace std;
 
 int main()
 {
-	// Sets all decimal numbers to be rounded to two decimal places.
-	std::cout << std::setprecision(2) << std::fixed;
+
 	
 	// Named constants for tip and tax rates.
 	const double TIP_PERCENTAGE = 0.20;
@@ -18,12 +17,15 @@ int main()
 		tip = subTotal * TIP_PERCENTAGE,
 		total = subTotal + tip;
 
+	// Sets all decimal numbers to be rounded to two decimal places.
+	cout << setprecision(2) << fixed;
 	// The use of setw aligns the decimals for the output of tax and tip.
-	cout << "Bill           $   " << restaurantBill << "\n";
-	cout << "Tax at 10.5%   $   " << setw(5) << tax << "\n";
-	cout << "Sub-total      $   " << subTotal << "\n";
-	cout << "Tip at 20%     $   " << setw(5) << tip << "\n\n";
-	cout << "Total Bill     $   " << total << "\n";
+	cout << "Bill           $ " << restaurantBill << "\n";
+	cout << "Tax at 10.5%   $ " << setw(5) << tax << "\n";
+	cout << "Sub-total      $ " << subTotal << "\n";
+	cout << "Tip at 20%     $ " << setw(5) << tip << "\n";
+	cout << "-----------------------" << "\n";
+	cout << "Total Bill     $ " << total << "\n\n";
 		
 
 	return 0;
